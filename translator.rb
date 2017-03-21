d269cc3fad7a40ecb35ec127f1dbaed0e77023e4
PolymathWhiz <anyanwumiracle78@gmail.com>
2017-03-21 09:22:33 +0000

    getting ready

--- translator.rb
+++ translator.rb
@@ -1,19 +1,34 @@
+require 'json'
+# include 'translationTwi'
+
 class Translator  
-  $word_list = { why:"aden", please:"m3pacho", commot:"Fa so hor", lets_go:"y3nko", or:"anaa", dumb:"wabon", come: "bra", go: "ko" }
+  $word_list = {
+                 why:"aden", 
+                 please:"m3pacho", 
+                 commot:"Fa so hor", 
+                 lets_go:"y3nko", 
+                 or:"anaa", 
+                 dumb:"wabon", 
+                 come: "bra", 
+                 go: "ko" 
+              }
   
   def to_twi(word)
 
-    if word.to_sym.equal?($word_list)
-      puts $word_list[word.to_sym]
-    else
-      puts "Word was not found in the list"
-    end
+    word = word.downcase.split.join('_')
+
+    # if word.to_sym.equal?($word_list)
+    #   puts $word_list[word.to_sym]
+    # else
+    #   puts "Word was not found in the list"
+    # end
+
+    puts word
 
   end
   
-  def underscore(word)
-    reg_ex = /\b\b/
-    puts word.gsub(reg_ex, "_")
-  end
-
 end
+
+translate = Translator.new
+
+translate.to_twi("Miracle Anyanwu")
